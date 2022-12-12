5dedbd07338775e22dc050e1a7ed40f0147ef083
Dan <me@danielholcomb.com>
2022-12-12 09:43:43 -0700

    Add results with collapse

--- Recipendium.UI/Pages/JustRecipeSearch.cshtml.cs
+++ Recipendium.UI/Pages/JustRecipeSearch.cshtml.cs
@@ -1,3 +1,4 @@
+using Core.HttpDynamo;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,36 @@ namespace Recipendium.UI.Pages
 {
     public class JustRecipeSearchModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public string Message { get; set; }
+        public List<string> ResultPages { get; set; }
+
+        public JustRecipeSearchModel(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task OnPost(string query)
+        {
+            Message = string.Empty;
+            if (!string.IsNullOrEmpty(query))
+            {
+                var lowQuery = query.ToLower();
+
+                if (!query.Contains("recipe"))
+                {
+                    if (!query.Contains("-recipe"))
+                    {
+                        query += " recipe";
+                        Message = "We noticed your search did not include recipe so we've included it for you. If you'd like to force search without the word recipe, please add '-recipe' to your search.";
+                    }
+                }
+
+                var results = await HttpDynamo.GetRequestAsync<List<string>>(_httpClientFactory, "https://recipendiumapi20221210174136.azurewebsites.net/Recipe?q=" + query);
+
+                ResultPages = results;
+            }
         }
     }
 }
