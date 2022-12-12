24bfee1cf382bdef7697f21634edcc423d8d152f
Dan <me@danielholcomb.com>
2022-12-12 08:51:32 -0700

    Initial commit

---
+++ Recipendium.UI/Pages/JustRecipeSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Recipendium.UI.Pages
+{
+    public class JustRecipeSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
